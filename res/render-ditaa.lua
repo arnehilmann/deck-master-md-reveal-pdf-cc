960950bc4109167ae3c5756219be0aeedc01b644
Arne Hilmann <arne@hilmann.de>
2017-11-08 13:48:38 +0100

    check for cached version

--- res/render-ditaa.lua
+++ res/render-ditaa.lua
@@ -2,12 +2,20 @@ function CodeBlock(elem)
     if elem.classes[1] == "ditaa" then
         local filetype = "png"
         local mimetype = "image/png"
+        local fname = "rendered/ditaa-" .. pandoc.sha1(elem.text) .. "." .. filetype
 
+        local f=io.open(fname,"r")
+        if f~=nil then
+            io.close(f)
+            io.stderr:write("cached ditaa rendering found!\n")
+            return nil
+        end
+
+        io.stderr:write("rendering ditaa\n")
         local img = pandoc.pipe("java", {"-jar", "lib/ditaa/service/web/lib/ditaa0_10.jar", "-", "-"}, elem.text)
-        local fname = "rendered/ditaa-" .. pandoc.sha1(img) .. "." .. filetype
-        file = io.open(fname, "w")
-        file:write(img)
-        file:close()
+        local f=io.open(fname, "w")
+        f:write(img)
+        f:close()
         pandoc.mediabag.insert(fname, mimetype, img)
         return pandoc.Para{ pandoc.Image({pandoc.Str("rendered ditaa diagram")}, fname) }
     end
--- res/render-plantuml.lua
+++ res/render-plantuml.lua
@@ -2,12 +2,20 @@ function CodeBlock(elem)
     if elem.classes[1] == "plantuml" then
         local filetype = "png"
         local mimetype = "image/png"
+        local fname = "rendered/plantuml-" .. pandoc.sha1(elem.text) .. "." .. filetype
 
+        local f=io.open(fname,"r")
+        if f~=nil then
+            io.close(f)
+            io.stderr:write("cached plantuml rendering found!\n")
+            return nil
+        end
+
+        io.stderr:write("rendering plantuml\n")
         local img = pandoc.pipe("java", {"-jar", "lib/plantuml.jar", "-tpng", "-p", "-Sbackgroundcolor=transparent"}, elem.text)
-        local fname = "rendered/plantuml-" .. pandoc.sha1(img) .. "." .. filetype
-        file = io.open(fname, "w")
-        file:write(img)
-        file:close()
+        f=io.open(fname, "w")
+        f:write(img)
+        f:close()
         pandoc.mediabag.insert(fname, mimetype, img)
         return pandoc.Para{ pandoc.Image({pandoc.Str("rendered plantuml diagram")}, fname) }
     end
